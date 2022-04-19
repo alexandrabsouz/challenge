7e0f06544512c7061ceb3c5a35664fde791f5f76
alexandra <alexandrabsouz@gmail.com>
2022-04-19 13:28:31 -0300

    feat: get repos delegate

--- lib/challenge.ex
+++ lib/challenge.ex
@@ -2,6 +2,12 @@ defmodule Challenge do
   alias Challenge.Users.Create, as: UserCreate
   alias Challenge.Users.Get, as: UserGet
 
+
+  alias Challenge.Github.Client, as: GithubClient
+
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate get_user_by_id(params), to: UserGet, as: :by_id
+
+
+  defdelegate get_repos(username), to: GithubClient, as: :get_repo_info
 end
