e85687c9d795e2d3d66468bbb788baeff261dce4
alexandra <alexandrabsouz@gmail.com>
2022-04-20 14:05:28 -0300

    fix: action fallback

--- lib/challenge_web/controllers/users_controller.ex
+++ lib/challenge_web/controllers/users_controller.ex
@@ -2,15 +2,32 @@ defmodule ChallengeWeb.UsersController do
     use ChallengeWeb, :controller
 
     alias Challenge.User
-    #alias ChallengeWeb.Auth.Guardian
     alias ChallengeWeb.FallbackController
 
 
+    action_fallback FallbackController
+
     def show(conn, %{"id" => id}) do
-        with {:ok, user} <- Challenge.get_user_by_id(id) do
+        with {:ok, %User{} = user} <- Challenge.get_user_by_id(id) do
           conn
           |> put_status(:ok)
           |> render("user.json", user: user)
         end
+    end
+
+    def create(conn, params) do
+      with {:ok, %User{} = user} <- Challenge.create_user(params) do
+        conn
+        |> put_status(:created)
+        |> render("create.json", user: user)
       end
+    end
+
+    def delete(conn, %{"id" => id}) do
+      with {:ok, %User{}} <- Challenge.delete_user(id) do
+        conn
+        |> put_status(:no_content)
+        |> text("")
+      end
+    end
 end
