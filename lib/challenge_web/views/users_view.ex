0e2d8ebd7744096f701716ef5c3510e24d081022
alexandra <alexandrabsouz@gmail.com>
2022-04-20 12:25:30 -0300

    feat: create user view

--- lib/challenge_web/views/users_view.ex
+++ lib/challenge_web/views/users_view.ex
@@ -1,9 +1,14 @@
 defmodule ChallengeWeb.UsersView do
     use ChallengeWeb, :view
 
-  def render("user.json", %{user: user}) do
-    %{
-      user: user
-    }
-  end
+    alias Challenge.User
+
+    def render("create.json", %{user: %User{} = user}) do
+      %{
+        message: "user created!",
+        user: user
+      }
+    end
+
+    def render("user.json", %{user: %User{} = user}), do: %{user: user}
 end
