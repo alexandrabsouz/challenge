08d2f08c6b5cd4ff85d0a9f9e67b6dc60fbba51c
alexandra <alexandrabsouz@gmail.com>
2022-04-20 14:04:42 -0300

    feat: create user

--- lib/challenge/users/create.ex
+++ lib/challenge/users/create.ex
@@ -1,13 +1,12 @@
 defmodule Challenge.Users.Create do
   alias Challenge.{Error, Repo, User}
-  alias Challenge.Github.Client
 
   def call(params) do
     username = Map.get(params, "username")
     changeset = User.changeset(params)
 
     with {:ok, %User{}} <- User.build(changeset),
-         {:ok, _body} <- Client.get_repo_info(username),
+         {:ok, _repo_info} <- client().get_repo_info(username),
          {:ok, %User{}} = user <- Repo.insert(changeset) do
       user
     else
@@ -15,6 +14,12 @@ defmodule Challenge.Users.Create do
       {:error, result} -> {:error, Error.build(:bad_request, result)}
     end
   end
+
+  defp client do
+    :challenge
+    |> Application.fetch_env!(__MODULE__)
+    |> Keyword.get(:github_client_adapter)
+  end
 end
 
-#user_params = %{"cpf" => "29534291072" , "password" => "teste1234", "username" => "alexandrabsouz"}
+#user_params = %{"cpf" => "29534291072" , "password" => "teste1234", "username" => "cccvcvcvcvcvcvcvcv"}
