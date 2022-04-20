1341de361f8fb7e56aab3c945f74ffaffefd9612
alexandra <alexandrabsouz@gmail.com>
2022-04-20 11:53:00 -0300

    fix: align routes

--- lib/challenge_web/router.ex
+++ lib/challenge_web/router.ex
@@ -21,18 +21,9 @@ defmodule ChallengeWeb.Router do
     pipe_through :api
 
     get "/:username/repos", ReposController, :show
-    
-    get "/users/:id", UsersController, :show
-    resources "/users", UsersController, except: [:new, :edit, :create]
+    resources "/users", UsersController, except: [:new, :edit]
   end
 
-  # Enables LiveDashboard only for development
-  #
-  # If you want to use the LiveDashboard in production, you should put
-  # it behind authentication and allow only admins to access it.
-  # If your application does not have an admins-only section yet,
-  # you can use Plug.BasicAuth to set up some basic authentication
-  # as long as you are also using SSL (which you should anyway).
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
