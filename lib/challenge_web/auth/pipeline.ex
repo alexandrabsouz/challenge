dcfc14aaae7347dfb1e6f46a7bd6ec2586998484
alexandra <alexandrabsouz@gmail.com>
2022-04-21 09:22:59 -0300

    docs: config pipeline auth

--- lib/challenge_web/auth/pipeline.ex
+++ lib/challenge_web/auth/pipeline.ex
@@ -1,7 +1,7 @@
 defmodule ChallengeWeb.Auth.Pipeline do
-  # use Guardian.Plug.Pipeline, otp_app: :challenge
+  use Guardian.Plug.Pipeline, otp_app: :challenge
 
-  # plug Guardian.Plug.VerifyHeader
-  # plug Guardian.Plug.EnsureAuthenticated
-  # plug Guardian.Plug.LoadResource
+  plug Guardian.Plug.VerifyHeader
+  plug Guardian.Plug.EnsureAuthenticated
+  plug Guardian.Plug.LoadResource
 end
