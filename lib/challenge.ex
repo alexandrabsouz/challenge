ad926aebb166920f2c8a45634017a91bf72d7d71
alexandra <alexandrabsouz@gmail.com>
2022-04-21 09:17:37 -0300

    feat: delegate update user

--- lib/challenge.ex
+++ lib/challenge.ex
@@ -2,6 +2,7 @@ defmodule Challenge do
   alias Challenge.Users.Create, as: UserCreate
   alias Challenge.Users.Delete, as: UserDelete
   alias Challenge.Users.Get, as: UserGet
+  alias Challenge.Users.Update, as: UserUpdate
 
 
   alias Challenge.Github.Client, as: GithubClient
@@ -9,6 +10,7 @@ defmodule Challenge do
   defdelegate create_user(params), to: UserCreate, as: :call
   defdelegate get_user_by_id(params), to: UserGet, as: :by_id
   defdelegate delete_user(params), to: UserDelete, as: :call
+  defdelegate update_user(params), to: UserUpdate, as: :call
 
 
   defdelegate get_repos(username), to: GithubClient, as: :get_repo_info
