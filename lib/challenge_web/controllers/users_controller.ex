4e2e54f105f621277a0a5f424195e945a073cd5f
alexandra <alexandrabsouz@gmail.com>
2022-04-21 09:26:58 -0300

    feat: signin controller

--- lib/challenge_web/controllers/users_controller.ex
+++ lib/challenge_web/controllers/users_controller.ex
@@ -3,6 +3,7 @@ defmodule ChallengeWeb.UsersController do
 
     alias Challenge.User
     alias ChallengeWeb.FallbackController
+    alias ChallengeWeb.Auth.Guardian
 
 
     action_fallback FallbackController
@@ -37,4 +38,13 @@ defmodule ChallengeWeb.UsersController do
         |> put_status(:ok)
         |> render("user.json", user: user)
       end
+    end
+
+    def sign_in(conn, params) do
+      with {:ok, token} <- Guardian.authenticate(params) do
+        conn
+        |> put_status(:ok)
+        |> render("sign_in.json", token: token)
+      end
+    end
 end
