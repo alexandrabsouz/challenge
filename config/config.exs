f186b13df772c0561d61a78cee507868082fdb76
alexandra <alexandrabsouz@gmail.com>
2022-04-21 09:20:27 -0300

    docs: config auth

--- config/config.exs
+++ config/config.exs
@@ -34,9 +34,9 @@ config :challenge, Challenge.Repo,
   migration_primary_key: [type: :binary_id],
   migration_foreign_key: [type: :binary_id]
 
-# config :challenge, ChallengeWeb.Auth.Pipeline,
-#   module: ChallengeWeb.Auth.Guardian,
-#   error_handler: ChallengeWeb.Auth.ErrorHandler
+config :challenge, ChallengeWeb.Auth.Pipeline,
+  module: ChallengeWeb.Auth.Guardian,
+  error_handler: ChallengeWeb.Auth.ErrorHandler
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
